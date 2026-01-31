d962ac062c79fbf11f1cd17514ab78f7f3707939
Andreas Jaekel <jaekel@cablecats.de>
2026-01-31 20:46:52 +0100

    fix zoom buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './styles.css'
 
 // Global styles
 document.body.style.margin = '0';
